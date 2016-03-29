7d454da66a27d7a44febdfad7a89c9f00cd0c027
James Skay <jskayjr@gmail.com>
2016-03-28 20:25:44 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	console.log("action jackson");
+
+	//get the input
+	$(".square").on("click", function(e) {
+		console.log("square", $(e.target).index());
+		console.log("game-segment", $(e.target).closest(".game-segment").index());
+	});
+
+		var state = 
+
+
+
+
+
+
+});
+
